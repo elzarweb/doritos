1cb97061baf495e0f29f530b44c727730030279c
Enrique Sanchez Jordan <enrique.sanchez@buongiorno.com>
2013-12-13 12:24:28 +0100

    cambio en clase borracho

--- src/Borracho.java
+++ src/Borracho.java
@@ -3,11 +3,14 @@ package src;
 public class Borracho {
 	
 	public void getBorracho(int num_cerves){
-		if(num_cerves<10){
+		if(num_cerves>0 && num_cerves<10){
 			System.out.println("No tienes que preocuparte, eres un español mas");
 		}else if (num_cerves>=10 && num_cerves<20){
 			System.out.println("Venga que tu puedes con alguna mas");
-		}else{
+		}else if(num_cerves == 0){
+			System.out.println("Ni llegaste al bar");
+		}
+		else{
 			System.out.println("vete a casa ya BORRACHO!!!!");
 		}
 	}
